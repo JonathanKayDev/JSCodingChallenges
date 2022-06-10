cd701375c6624a3d055b2115bf963d20803fcc99
JonathanKayDev <67706563+JonathanKay86@users.noreply.github.com>
2022-06-10 08:36:54 +0100

    Update site07.js
    solve challenge 7

--- Challenge07/site07.js
+++ Challenge07/site07.js
@@ -28,6 +28,23 @@ function checkPalindrome() {
 //takes an string to check if it is a palindrome.
 //returns true or false. 
 function isPalindrome(str) {
+    // convert to lower case
+    str = str.toLowerCase();
+    // strip out white space and non-alphanumeric characters
+    str = str.replace(/[^a-z0-9]/g,"");
+
+    // reverse string in a for loop
+    let reversedStr = "";
+
+    for (let index = str.length -1; index >= 0; index--) {
+        reversedStr += str[index];
+    }
+
+    // see if input and reversed string are the same
+    if (str == reversedStr) {
+        return true;
+    }
+
 
     return false;
 }
